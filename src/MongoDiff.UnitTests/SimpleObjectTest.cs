353079c1e35faf73f3474541d810cfc8b6cc719f
Lukas Angerer <executor.dev@gmail.com>
2024-09-03 21:05:51 +0200

    More tests for basic entity diff

--- src/MongoDiff.UnitTests/SimpleObjectTest.cs
+++ src/MongoDiff.UnitTests/SimpleObjectTest.cs
@@ -30,6 +30,21 @@ public class SimpleObjectTest
     }
 
     [Fact]
+    public void EntityDiff_WithIdChange_ContainsAllChanges()
+    {
+        var entity = BsonSerializer.Deserialize<Employee>(File.ReadAllText("./test-data/employee.bson"));
+        var left = entity.ToBsonDocument();
+        entity.Id = ObjectId.GenerateNewId();
+        var right = entity.ToBsonDocument();
+        var diff = EntityDiff.Build(left, right);
+
+        diff.Changes.Should().HaveCount(1);
+        var change = diff.Changes.Single();
+        change.OldValue.Should().Be(ObjectId.Parse("66c7091b4b5d3018e3cda214"));
+        change.NewValue.Should().Be(entity.Id);
+    }
+
+    [Fact]
     public void EntityDiff_WithMultipleChanges_ContainsAllChanges()
     {
         var entity = BsonSerializer.Deserialize<Employee>(File.ReadAllText("./test-data/employee.bson"));
@@ -44,5 +59,25 @@ public class SimpleObjectTest
         diff.Changes.Should().HaveCount(4);
         diff.Changes.Select(c => c.Path).Order().Should()
             .ContainInOrder(["branches", "dateOfBirth", "lastname", "teams"]);
+
+        var branchesChange = diff.Changes.Single(c => c.Path == "branches");
+        branchesChange.OldValue.Should().BeAssignableTo<IEnumerable<BsonValue>>()
+            .Which.Should().BeEmpty();
+        branchesChange.NewValue.Should().BeAssignableTo<IEnumerable<BsonValue>>()
+            .Which.Select(v => v.AsString).Should().BeEquivalentTo(entity.Branches);
+
+        var dateOfBirthChange = diff.Changes.Single(c => c.Path == "dateOfBirth");
+        dateOfBirthChange.OldValue.Should().BeNull();
+        dateOfBirthChange.NewValue.Should().Be("1980-01-01");
+
+        var lastnameChange = diff.Changes.Single(c => c.Path == "lastname");
+        lastnameChange.OldValue.Should().Be("Angerer");
+        lastnameChange.NewValue.Should().Be("Osteron");
+
+        var teamsChange = diff.Changes.Single(c => c.Path == "teams");
+        teamsChange.OldValue.Should().BeAssignableTo<IEnumerable<BsonValue>>()
+            .Which.Select(v => v.AsString).Should().BeEquivalentTo("Alpha");
+        teamsChange.NewValue.Should().BeAssignableTo<IEnumerable<BsonValue>>()
+            .Which.Select(v => v.AsString).Should().BeEquivalentTo(entity.Teams);
     }
 }
